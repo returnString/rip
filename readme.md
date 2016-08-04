39eb35f13399da1678d5c6974338d3a79224199f
Ruan Pearce-Authers <ruanpa@outlook.com>
2016-08-04 22:21:35 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+# rip
+Simple package management for R.
+
+# Commands
+- `rip init` initialises an empty manifest
+- `rip install <package>` installs a package and records it in your manifest
+- `rip restore` installs all packages referenced in your manifest
---
+++ rip.r
@@ -0,0 +1,97 @@
+#!/usr/bin/env RScript
+
+repoConfig <- getOption("repos")
+repoConfig["CRAN"] = "http://cran.uk.r-project.org"
+options(repos = repoConfig)
+
+manifestName <- '.rip'
+args <- commandArgs(trailingOnly = T)
+
+assertPwd <- function(isRipProject)
+{
+	if (isRipProject != file.exists(manifestName))
+	{
+		stop(paste("Current directory", if (isRipProject) "is not" else "is already" , "a rip project"))
+	}
+}
+
+loadPackages <- function()
+{
+	fh <- file(manifestName, open = "r")
+	packages <- c()
+	while (length(line <- readLines(fh, n = 1)) > 0)
+	{
+		packages <- c(packages, line);
+	}
+	close(fh)
+	return(packages)
+}
+
+savePackages <- function(packages)
+{
+	fh <- file(manifestName, open = "w")
+	writeLines(sort(packages), fh)
+	close(fh)
+}
+
+restore <- function()
+{
+	assertPwd(T)
+	packages <- loadPackages()
+
+	if (!length(packages))
+	{
+		stop("No packages in manifest to restore")
+	}
+
+	install.packages(packages)
+}
+
+install <- function()
+{
+	assertPwd(T)
+	package <- args[2]
+	
+	if (is.na(package))
+	{
+		stop("Please specify a package")
+	}
+
+	if (!(package %in% available.packages()[,"Package"]))
+	{
+		stop("Package is not available in the repository")
+	}
+
+	packages <- loadPackages()
+	if (package %in% packages)
+	{
+		stop("Package is already installed")
+	}
+
+	install.packages(package)
+	savePackages(c(packages, package))
+}
+
+init <- function()
+{
+	assertPwd(F)
+	file.create(manifestName)
+}
+
+invalidCommand <- function()
+{
+	stop("Invalid command")
+}
+
+main <- function()
+{
+	command <- switch(args[1],
+		init = init,
+		install = install,
+		restore = restore,
+		invalidCommand)
+
+	command()
+}
+
+main()
